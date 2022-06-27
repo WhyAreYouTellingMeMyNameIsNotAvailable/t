95137883c0630a874d5c7b962c98837dd0fc22c8
WhyAreYouTellingMeMyNameIsNotAvailable <108173194+WhyAreYouTellingMeMyNameIsNotAvailable@users.noreply.github.com>
2022-06-28 05:38:29 +0800

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,92 @@
 import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-    public class Main {
+
+        public class Main{
         static Scanner sc = new Scanner(System.in);
         static char operation;
         static int num, num1;
         static int result;
-
-
-        public static void main(String[] args){
-            System.out.println("Введите выражение римскими или арабскими цифрами от 1 до 10:");
-            String x = sc.nextLine();
-            char[] ch = new char[10];
-            for (int i = 0; i < x.length(); i++) {
-
-                ch[i] = x.charAt(i);
-                if (ch[i] == '+') {
-                    operation = '+';
+            public static void main(String[] args) {
+                String[] rom = {"X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I"};
+                System.out.println("Введите выражение римскими или арабскими цифрами от 1 до 10:");
+                String x = sc.nextLine();
+                char[] ch = new char[10];
+                char pl = '+';
+                char mi = '-';
+                char zv = '*';
+                char pal = '/';
+                int co = 0, cou = 0, coun = 0, count = 0;
+                for (int i = 0; i < x.length(); i++) {
+                    if (x.charAt(i) == pl) {
+                        co++;
+                    }
                 }
-                if (ch[i] == '-') {
-                    operation = '-';
+                for (int ii = 0; ii < x.length(); ii++) {
+                    if (x.charAt(ii) == mi) {
+                        cou++;
+                    }
                 }
-                if (ch[i] == '*') {
-                    operation = '*';
+                for (int iii = 0; iii < x.length(); iii++) {
+                    if (x.charAt(iii) == zv) {
+                        coun++;
+                    }
                 }
-                if (ch[i] == '/') {
-                    operation = '/';
+                for (int iiii = 0; iiii < x.length(); iiii++) {
+                    if (x.charAt(iiii) == pal) {
+                        count++;
+                    }
                 }
-            }
-            String chS = String.valueOf(ch);
-            String[] spl = chS.split("[+-/*]");
-
-            String a = spl[0];
-            String b = spl[1];
-            String c = b.trim();
-            num = romToNum(a);
-            num1 = romToNum(c);
-            if (num < 0 && num1 < 0) {
-                result = 0;
-            } else {
-                result = calculated(num, num1, operation);
-                String resultRom = numToRom(result);
-                System.out.println(resultRom);
-            }
-            num = Integer.parseInt(a);//преобразует строку в число
-            num1 = Integer.parseInt(c);
-            result = calculated(num, num1, operation);
-
-
-            if (num % 1 != 0 && num1 % 1 != 0) {
-                try {
-                    throw new IOException();
-                } catch (IOException e) {
-                    System.out.println("Введены не целые числа.");
+                for (int i = 0; i < x.length(); i++) {
+                    ch[i] = x.charAt(i);
+                    if (ch[i] == '+' && co == 1) {
+                        operation = '+';
+                    } else if (ch[i] == '-' && cou == 1) {
+                        operation = '-';
+                    } else if (ch[i] == '*' && coun == 1) {
+                        operation = '*';
+                    } else if (ch[i] == '/' && count == 1) {
+                        operation = '/';
+                    }
                 }
-            } else if (num == 0 && num1 == 0) {
-                try {
-                    throw new IOException();
-                } catch (IOException e) {
-                    System.out.println("Похоже, оба числа равны нулю. Попробуйте еще раз.");
-
+                String chS = String.valueOf(ch);
+                String[] spl = chS.split("[+-/*]");
+                String a = spl[0];
+                String b = spl[1];
+                String c = b.trim();
+                boolean smth = false;
+                for (String s : rom) {
+                    if (s.equals(spl[0]) || s.equals(spl[1])) {
+                        smth = true;
+                        break;
+                    }
                 }
-            } else if (num <= 10 && num1 > 10 || num > 10 && num1 <= 10 || num > 10 && num1 > 10) {
-                try {
-                    throw new IOException();
-                } catch (IOException e) {
-                    System.out.println("Пожалуйста, введите два целых числа от 1 до 10.");
+                if (smth) {
+                    num = romToNum(a);
+                    num1 = romToNum(c);
+                    if (num < 0 && num1 < 0) {
+                        result = 0;
+                    } else {
+                        result = calc(num, num1, operation);
+                        if (num > 10 || num1 > 10) {
+                            System.out.print("Неверное значение");
+                        } else {
+                            String resultRoman = numToRom(result);
+                            System.out.println(resultRoman);
+                        }
+                    }
+                } else {
+                    num = Integer.parseInt(a);
+                    num1 = Integer.parseInt(c);
+                    result = calc(num, num1, operation);
+                    if (num > 10 || num1 > 10) {
+                        System.out.print("Неверное значение");
+                    } else {
+                        String r = Integer.toString(result);
+                        System.out.println(r);
+                    }
                 }
-            } else {
-                System.out.println(result);
-            }
-        }
-
-
+                    }
             public static String numToRom(int numArabian) {
                 String[] rom = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
                         "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
@@ -83,41 +97,39 @@ import java.util.Scanner;
                         "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
                         "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
                         "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
-                        "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
-                };
+                        "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
                 final String s = rom[numArabian];
-                    return s;
+                return s;
             }
-        public static int romToNum(String rom) {
-            try {
-                if (rom.equals("I")) {
-                    return 1;
-                } else if (rom.equals("II")) {
-                    return 2;
-                } else if (rom.equals("III")) {
-                    return 3;
-                } else if (rom.equals("IV")) {
-                    return 4;
-                } else if (rom.equals("V")) {
-                    return 5;
-                } else if (rom.equals("VI")) {
-                    return 6;
-                } else if (rom.equals("VII")) {
-                    return 7;
-                } else if (rom.equals("VIII")) {
-                    return 8;
-                } else if (rom.equals("IX")) {
-                    return 9;
-                } else if (rom.equals("X")) {
-                    return 10;
+            public static int romToNum(String rom) {
+                try {
+                    if (rom.equals("I")) {
+                        return 1;
+                    } else if (rom.equals("II")) {
+                        return 2;
+                    } else if (rom.equals("III")) {
+                        return 3;
+                    } else if (rom.equals("IV")) {
+                        return 4;
+                    } else if (rom.equals("V")) {
+                        return 5;
+                    } else if (rom.equals("VI")) {
+                        return 6;
+                    } else if (rom.equals("VII")) {
+                        return 7;
+                    } else if (rom.equals("VIII")) {
+                        return 8;
+                    } else if (rom.equals("IX")) {
+                        return 9;
+                    } else if (rom.equals("X")) {
+                        return 10;
+                    }
+                } catch (InputMismatchException e) {
+                    throw new InputMismatchException("Неверный формат.");
                 }
-            } catch (InputMismatchException e) {
-                throw new InputMismatchException("Неверный формат.");
+                return -1;
             }
-            return -1;
-        }
-
-            public static int calculated ( int num, int num1, char op){
+            public static int calc(int num, int num1, char op) {
                 int result = 0;
                 switch (op) {
                     case '+':
@@ -140,11 +152,10 @@ import java.util.Scanner;
                     default:
                         try {
                             throw new IllegalArgumentException();
-                        }catch (IllegalArgumentException e){
+                        } catch (IllegalArgumentException e) {
                             System.out.println("Не верный знак операции.");
                         }
-
                 }
-            return result;
+                return result;
+            }
         }
-    }
